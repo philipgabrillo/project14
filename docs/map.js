92d4932f1f365ac20a13f82c5f5476d2e22b84cd
philipgabrillo <133919146+philipgabrillo@users.noreply.github.com>
2023-08-21 23:00:23 -0400

    Add files via upload

---
+++ docs/map.js
@@ -0,0 +1,40 @@
+const map = L.map('map').setView(
+    [27.773056, // latitude
+    -82.639999], // longitude
+    13  // zoom
+);
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([27.751, -82.639999]).addTo(map);
+var circle = L.circle([27.773056, -82.639999], {radius: 1000}).addTo(map);
+
+// Define coordinates for the polygon (you need to provide them)
+var latlong = [
+    [27.7991534,-82.6407864],
+    [27.7998574,-82.6706215],
+    [27.773056, -82.639999]
+];
+
+var polygon = L.polygon(latlong, {
+    color: 'orange',
+    fillOpacity: 0.8,
+    weight: 6
+}).addTo(map);
+
+var popup = L.popup()
+    .setLatLng([27.773056, -82.639999])
+    .setContent("Test pop.")
+    .openOn(map);
+    
+marker.bindPopup("Test pop.").openPopup();
+circle.bindPopup("<b>Testing!</b>I am a circle.");
+polygon.bindPopup("Testing a polygon.");
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
---
+++ docs/script.js
@@ -0,0 +1,30 @@
+function bubbleSort(arr) {
+  var n = arr.length;
+  var swapped;
+
+  do {
+      swapped = false;
+      for (var i = 0; i < n - 1; i++) {
+          if (arr[i] > arr[i + 1]) {
+              // Swap elements arr[i] and arr[i+1]
+              var temp = arr[i];
+              arr[i] = arr[i + 1];
+              arr[i + 1] = temp;
+              swapped = true;
+          }
+      }
+  } while (swapped);
+
+  return arr;
+}
+
+function sortArray() {
+  var input = document.getElementById("arrayInput").value;
+  var arr = input.split(",").map(function(item) {
+      return parseInt(item.trim());
+  });
+
+  var sortedArray = bubbleSort(arr);
+
+  document.getElementById("output").innerText = "Sorted Array: " + sortedArray.join(", ");
+}
